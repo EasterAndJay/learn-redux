cad6d717211f2dee04d4d279dd58689d517ce971
Jonathan Easterman <jonathaneasterman@gmail.com>
2016-06-28 14:07:33 -0700

    First commit of basic app to become familiar with Redux

---
+++ redux-book-list/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript: The Good Parts', pages: 101},
+    {title: "Harry Potter", pages: 300},
+    {title: "Boiling Point", pages: 267},
+    {title: "Redux React", pages: 107}
+  ];
+}
